b83270b04855577c517d22a2521c972bdd2e868a
Ellaine Rivera <ellainejoycerrivera@iskolarngbayan.pup.edu.ph>
2024-02-09 22:02:48 +0800

    reupload

--- guess_a_number.py
+++ guess_a_number.py
@@ -10,3 +10,19 @@ if top_of_range.isdigit ():
     if top_of_range <= 0:
         print ("Next time, choose number that is greater than 0.")
         quit ()
+
+else:
+    print('Please type a number next time.')
+    quit()
+
+random_number = random.randint(0, top_of_range)
+guesses = 0
+
+while True:
+    guesses += 1
+    user_guess = input("Make a guess: ")
+    if user_guess.isdigit():
+        user_guess = int(user_guess)
+    else:
+        print('Please type a number next time.')
+        continue
